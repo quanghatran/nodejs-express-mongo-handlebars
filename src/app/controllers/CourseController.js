eb20a08388af29987a77f01aac0ab9693a955fd7
quanghatran <quanghatran2161998@gmail.com>
2022-01-20 22:17:13 +0700

    add create new course page

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -10,6 +10,24 @@ class CourseController {
 			)
 			.catch(next);
 	}
+
+	// [GET] /course/create
+	create(req, res, next) {
+		res.render("courses/create");
+	}
+
+	// [POST] /course/store
+	store(req, res, next) {
+		const formData = req.body;
+		formData.image = `https://www.youtube.com/watch?v=${req.body.videoId}`;
+		const course = new Course(formData);
+		course
+			.save()
+			.then(() => res.redirect("/"))
+			.catch((err) => console.log("ERROR: ", err));
+
+		// res.send("course saved!!!");
+	}
 }
 
 module.exports = new CourseController();
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,14 +1,21 @@
 const mongoose = require("mongoose");
+const slug = require("mongoose-slug-generator");
 const Schema = mongoose.Schema;
 
-const Course = new Schema({
-	name: { type: String, maxLength: 255 },
-	description: { type: String, maxLength: 600 },
-	slug: { type: String, maxLength: 255 },
-	image: { type: String, maxLength: 255 },
-	videoId: { type: String, maxLength: 255 },
-	createdAt: { type: Date, default: Date.now() },
-	updatedAt: { type: Date, default: Date.now() },
-});
+mongoose.plugin(slug);
+
+const Course = new Schema(
+	{
+		name: { type: String, require: true },
+		description: { type: String },
+		slug: { type: String },
+		image: { type: String, require: true },
+		videoId: { type: String, require: true },
+		slug: { type: String, slug: "name", unique: true },
+	},
+	{
+		timestamps: true,
+	}
+);
 
 module.exports = mongoose.model("Course", Course);
